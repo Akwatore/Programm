3a5b29556340fa442126d3a1a6db999e26c25529
Plyaskina Ulyana <31893521+Akwatore@users.noreply.github.com>
2020-06-29 11:19:42 +0300

    Create ISR_4.2.py

---
+++ Sem_5/ISR_4.2.py
@@ -0,0 +1,30 @@
+import pyqrcode
+from PIL import Image, ImageDraw
+
+def text_to_qr(url, filename, *, scale=500, quality=100):
+    qr = pyqrcode.create(url).text().splitlines()
+
+    width = len(qr[0])
+    height = len(qr)
+
+    im = Image.new(mode='RGB', size=(width, height))
+    draw = ImageDraw.Draw(im)
+
+    for y in range(height):
+        for x in range(width):
+            draw.point(
+                xy=(x, y),
+                fill=tuple([int(qr[y][x]) * 255 for i in range(3)])
+                )
+    im = im.resize((scale, scale))
+    im.save('qr.jpg', quality=quality)
+
+
+text_to_qr(
+    'hi',
+    filename='qr.jpg'
+    )
+
+
+#Ссылка на repl
+#https://repl.it/@ulyaakwatore/Siem-5-Tiema-4-ISR-42#main.py
